81f3ee380e5e25980a5751a55267a423dfb7fbb1
22021538 <22021538@myrp.edu.sg>
2023-08-15 17:22:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22021538/mywebbrowser/MainActivity.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c346.id22021538.mywebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLoadURL;
+    WebView wvMyPage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        wvMyPage.setWebViewClient(new WebViewClient());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.rp.edu.sg/soi/home";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }};
